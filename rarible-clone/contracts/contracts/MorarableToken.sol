b250c56d73a189b2e9fb41033b1f152569c77e22
Broncoteam <101840462+Broncoteam@users.noreply.github.com>
2022-05-11 21:05:38 +0300

    Update MorarableToken.sol

--- rarible-clone/contracts/contracts/MorarableToken.sol
+++ rarible-clone/contracts/contracts/MorarableToken.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.0;
 import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
 
-contract MorarableToken is ERC721 {
+contract BroncoToken is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor () ERC721("MorarableToken", "MORA"){}
+    constructor () ERC721("BroncoToken", "BRNC"){}
 
     struct Item {
         uint256 id;
